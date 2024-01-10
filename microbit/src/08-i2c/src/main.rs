2bace797cc03425cafe837e23083f1dae0500f47
Eric Wolf <ericwolf42@gmail.com>
2024-01-10 08:32:26 +0100

    Read the accelerometer

--- microbit/src/08-i2c/src/main.rs
+++ microbit/src/08-i2c/src/main.rs
@@ -3,34 +3,21 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use rtt_target::{rtt_init_print, rprintln};
 use panic_rtt_target as _;
-
-use microbit::hal::prelude::*;
+use rtt_target::{rprintln, rtt_init_print};
 
 #[cfg(feature = "v1")]
-use microbit::{
-    hal::twi,
-    pac::twi0::frequency::FREQUENCY_A,
-};
+use microbit::{hal::twi, pac::twi0::frequency::FREQUENCY_A};
 
 #[cfg(feature = "v2")]
-use microbit::{
-    hal::twim,
-    pac::twim0::frequency::FREQUENCY_A,
-};
+use microbit::{hal::twim, pac::twim0::frequency::FREQUENCY_A};
 
-const ACCELEROMETER_ADDR: u8 = 0b0011001;
-const MAGNETOMETER_ADDR: u8 = 0b0011110;
-
-const ACCELEROMETER_ID_REG: u8 = 0x0f;
-const MAGNETOMETER_ID_REG: u8 = 0x4f;
+use lsm303agr::{AccelOutputDataRate, Lsm303agr};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let board = microbit::Board::take().unwrap();
-
 
     #[cfg(feature = "v1")]
     let mut i2c = { twi::Twi::new(board.TWI0, board.i2c.into(), FREQUENCY_A::K100) };
@@ -38,15 +25,13 @@ fn main() -> ! {
     #[cfg(feature = "v2")]
     let mut i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
 
-    let mut acc = [0];
-    let mut mag = [0];
-
-    // First write the address + register onto the bus, then read the chip's responses
-    i2c.write_read(ACCELEROMETER_ADDR, &[ACCELEROMETER_ID_REG], &mut acc).unwrap();
-    i2c.write_read(MAGNETOMETER_ADDR, &[MAGNETOMETER_ID_REG], &mut mag).unwrap();
-
-    rprintln!("The accelerometer chip's id is: {:#b}", acc[0]);
-    rprintln!("The magnetometer chip's id is: {:#b}", mag[0]);
-
-    loop {}
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
+    sensor.init().unwrap();
+    sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
+    loop {
+        if sensor.accel_status().unwrap().xyz_new_data {
+            let data = sensor.accel_data().unwrap();
+            rprintln!("Accel: x {} y {} z {}", data.x, data.y, data.z);
+        }
+    }
 }
